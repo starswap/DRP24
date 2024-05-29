46a00054e08b8c68071092cfd0f0df1cd3e1f8d2
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-05-29 18:59:56 +0100

    Serve on static port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.port;
+
 //Express Middleware
 app.use(express.json());
 app.use(express.static('../frontend/build')); //serve statically deployed frontend after React compilation
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
