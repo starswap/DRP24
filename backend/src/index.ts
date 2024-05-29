f5803f133e420debef48d7e559fe83b472a8b7cb
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-05-29 19:46:54 +0100

    Fix frontend path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,13 @@ import express from 'express';
 const app = express();
 
 const PORT = process.env.PORT || 80;
+const FRONTEND_PATH = '../../frontend/build';
 
-//Express Middleware
+/* Express Middleware */
+// Support JSON incoming to the endpoints
 app.use(express.json());
-app.use(express.static('../frontend/build')); //serve statically deployed frontend after React compilation
+// Serve statically deployed frontend after React compilation
+app.use(express.static(FRONTEND_PATH));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
