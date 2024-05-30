ace992dfb9c006313b82f492197b4cf1c9a9aa20
Luke Moran <lm1222@ic.ac.uk>
2024-05-30 21:40:00 +0100

    fix(frontend): :bug: Fix path to CalendarEvent type

--- frontend/src/services/createEvent.ts
+++ frontend/src/services/createEvent.ts
@@ -1,6 +1,6 @@
 import { db } from '../firebase';
 import { setDoc, doc } from 'firebase/firestore';
-import { CalendarEvent } from 'common/types/CalendarEvent'
+import { CalendarEvent } from '../../../common/types/CalendarEvent';
 
 function createEvent(event: CalendarEvent) {
   setDoc(doc(db, 'events'), event);
