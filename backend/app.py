c96e9f9af3bc04b87e98a976fcc9f0d80e503bf4
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-06-04 17:01:47 +0100

    Fix static path

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,7 @@ from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
-app.wsgi_app.add_files("../frontend/build")
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="../frontend/build")
 
 @app.route("/api/test")
 def hello_world():
