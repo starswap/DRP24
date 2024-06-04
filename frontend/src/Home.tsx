509cc4cf5eaace759c204474eb3586b526107e49
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-06-04 18:12:03 +0100

    Switch to hash router

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>Congrats you have reached the home page:</h1>
-      <a className="underline text-blue-400" href="./create">
+      <a className="underline text-blue-400" href="#create">
         Create event
       </a>
     </>
