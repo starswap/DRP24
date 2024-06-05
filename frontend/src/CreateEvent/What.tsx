48779b6d558d2ad89fdac2eb5e287de3719b5e6c
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-06-05 13:05:58 +0100

    Make green when activity chosen

--- frontend/src/CreateEvent/What.tsx
+++ frontend/src/CreateEvent/What.tsx
@@ -7,8 +7,8 @@ export function What({
   state: calevent,
   updateState: updateActivity
 }: MultiPageFormStateProps<CalendarEvent>) {
-  const ROW1 = ['Walk', 'Cricket', 'Bingo'];
-  const ROW2 = ['Poker', 'Cooking', 'Coffee'];
+  const ACTIVITIES = ['Walk', 'Cricket', 'Bingo', 'Poker', 'Cooking', 'Coffee'];
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const saveActivity = (event: any) => {
     updateActivity((calevent) => ({
@@ -20,21 +20,17 @@ export function What({
   return (
     <>
       <h1>What will you be doing?</h1>
-      <div className="grid grid-rows-2">
-        <div className="flex">
-          {ROW1.map((v) => (
-            <ThemeButton key={v} onClick={saveActivity} value={v}>
-              {v}
-            </ThemeButton>
-          ))}
-        </div>
-        <div className="flex">
-          {ROW2.map((v) => (
-            <ThemeButton key={v} onClick={saveActivity} value={v}>
-              {v}
-            </ThemeButton>
-          ))}
-        </div>
+      <div className="grid grid-cols-3">
+        {ACTIVITIES.map((v) => (
+          <ThemeButton
+            key={v}
+            onClick={saveActivity}
+            value={v}
+            className={v === calevent.activity ? 'bg-green-400' : ''}
+          >
+            {v}
+          </ThemeButton>
+        ))}
       </div>
       <ThemeTextbox
         placeholder={'Or enter custom:'}
