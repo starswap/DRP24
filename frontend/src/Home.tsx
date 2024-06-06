941341eb9db9b2aef239373e6a56baeab9d318cb
Tom Underhill <tomunderhill6@gmail.com>
2024-06-06 12:04:24 +0100

    Dispaly event in a nice way and colour the names based on responses

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -3,6 +3,7 @@ import { ThemeSubheading } from './theme/ThemeSubheading';
 import { CURRENT_USER, fetchEvents } from './util/data';
 import { CalendarEvent, EventResponse } from './types/CalendarEvent';
 import { UID } from './types/UID';
+import dayjs, { Dayjs } from 'dayjs';
 
 export default function Home() {
   const [events, setEvents] = useState<[CalendarEvent, UID][]>([]);
@@ -11,6 +12,20 @@ export default function Home() {
     fetchEvents(CURRENT_USER).then(setEvents);
   }, []);
 
+  // function getPeople (statuses: {
+  //   [k: string]: Status}) {
+  //     return ()
+  //   }
+  function GetResponseColour(response: EventResponse) {
+    switch (response) {
+      case EventResponse.ACCEPTED:
+        return 'green';
+      case EventResponse.REJECTED:
+        return 'red';
+      case EventResponse.UNKNOWN:
+        return 'grey';
+    }
+  }
   console.log(events);
   return (
     <div className="flex flex-col items-center">
@@ -18,12 +33,37 @@ export default function Home() {
 
       <ThemeSubheading>Invites</ThemeSubheading>
       {events
-        .filter(
-          ([event]) =>
-            event.statuses[CURRENT_USER].response === EventResponse.UNKNOWN
-        )
-        .map(([event, uid]) => (
-          <p key={uid}>{event.activity}</p>
+        // .filter(
+        //   ([event]) =>
+        //     event.statuses[CURRENT_USER].response === EventResponse.UNKNOWN
+        // )
+        .map(([event, event_uid]) => (
+          <p key={event_uid}>
+            {event.activity} at {event.location} with{' '}
+            {Object.entries(event.statuses)
+              // dont display self
+              // .filter(([uid, status]) => uid !== CURRENT_USER)
+              // only display people who accepted
+              // .filter(
+              //   ([uid, status]) => status.response === EventResponse.ACCEPTED
+              // )
+              .map(
+                ([uid, status]) => (
+                  <span
+                    key={uid}
+                    style={{ color: GetResponseColour(status.response) }}
+                  >
+                    {status.person.name.firstname}{' '}
+                  </span>
+                )
+                // status.person.name.firstname +
+                // ' ' +
+                // status.person.name.surname
+              )}
+            {/* .join({', '})} */}
+            {/* // .join(', ')}{' '} */}
+            at {dayjs(event.time).format('YYYY-MM-DD HH:mm')}
+          </p>
         ))}
 
       <ThemeSubheading>Events</ThemeSubheading>
