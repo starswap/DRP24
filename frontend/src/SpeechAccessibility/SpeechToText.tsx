88b1dc3b39b67e7bff8cc52e1e74f702c2f0e9bc
TomSmail <thomas.c.smail@gmail.com>
2024-06-06 12:21:12 +0100

    fix: add blob url to onStop function

--- frontend/src/SpeechAccessibility/SpeechToText.tsx
+++ frontend/src/SpeechAccessibility/SpeechToText.tsx
@@ -6,19 +6,15 @@ import { CalendarEvent } from '../types/CalendarEvent';
 
 const URL = 'http://127.0.0.1:5000/';
 
-interface AudioBlobProps {
-  audioBlobURL: string | null;
-}
-
 // Function packages audio blob and sends it to globally defined URL
-async function uploadAudio(props: AudioBlobProps) {
+async function uploadAudio(blobUrl: string) {
   console.log('Uploading');
 
   // Construct the form data to be sent
   const formData = new FormData();
-  if (props.audioBlobURL !== null) {
-    console.log(props.audioBlobURL);
-    const audioBlob = await fetch(props.audioBlobURL).then((r) => r.blob());
+  if (blobUrl !== null) {
+    console.log(blobUrl);
+    const audioBlob = await fetch(blobUrl).then((r) => r.blob());
     formData.append('audio', audioBlob);
   } else {
     console.log('Error: blob url not present');
@@ -49,15 +45,14 @@ export function AudioRecordButton({
 }: {
   activitySetter: (a: CalendarEvent) => void;
 }) {
-  const {
-    startRecording,
-    stopRecording,
-    mediaBlobUrl,
-    status,
-    error,
-    isAudioMuted
-  } = useReactMediaRecorder({ audio: true });
-  console.log(isAudioMuted);
+  const { startRecording, stopRecording } = useReactMediaRecorder({
+    audio: true,
+    onStop: (blobUrl) => {
+      console.log(blobUrl);
+      const response = uploadAudio(blobUrl);
+      console.log(`Server Response: ${response}`);
+    }
+  });
 
   const [recording, setRecording] = useState(false);
   const handleClick = () => {
@@ -66,9 +61,6 @@ export function AudioRecordButton({
       console.log('Stopping Recording');
       stopRecording();
       setRecording(false);
-      const audioBlobProps: AudioBlobProps = { audioBlobURL: mediaBlobUrl };
-      const response = uploadAudio(audioBlobProps);
-      console.log(`Server Response: ${response}`);
     } else {
       // Start
       console.log('Starting Recording');
