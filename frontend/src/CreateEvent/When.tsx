1f863e59495b3f2396dd27d6001a1b066bf8877f
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-06-06 11:32:58 +0100

    Delete redundant CSS and classes

--- frontend/src/CreateEvent/When.tsx
+++ frontend/src/CreateEvent/When.tsx
@@ -1,26 +1,22 @@
 import { CalendarEvent } from '../types/CalendarEvent';
-import DateTimePicker from 'react-datetime-picker';
 import { MultiPageFormStateProps } from '../MultiPageForm/MultiPageForm';
-import './When.css';
 import dayjs from 'dayjs';
 
 export function When({
   state: calevent,
   updateState: updateActivity
 }: MultiPageFormStateProps<CalendarEvent>) {
-  const onSetDate = (date: Date | null) => {
-    if (date) {
-      const new_acc = {
-        ...calevent,
-        time: date
-      };
-      updateActivity(new_acc);
-    }
+  const onSetDate = (date: Date) => {
+    const new_acc = {
+      ...calevent,
+      time: date
+    };
+    updateActivity(new_acc);
   };
   return (
-    <div className="when-container">
+    <div>
       <h1>When will you be doing it?</h1>
-      <div className="datetime-picker-container">
+      <div>
         <input
           type="datetime-local"
           value={dayjs(calevent.time).format('YYYY-MM-DDThh:mm')}
