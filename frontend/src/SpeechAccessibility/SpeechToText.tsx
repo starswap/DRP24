44ddd297a536a3df0c1f3bd789469d5b8878732d
TomSmail <thomas.c.smail@gmail.com>
2024-06-06 10:57:11 +0100

    fix: remove test code

--- frontend/src/SpeechAccessibility/SpeechToText.tsx
+++ frontend/src/SpeechAccessibility/SpeechToText.tsx
@@ -10,28 +10,20 @@ interface AudioBlobProps {
   audioBlobURL: string | null;
 }
 
+// Function packages audio blob and sends it to globally defined URL
 async function uploadAudio(props: AudioBlobProps) {
   console.log('Uploading');
 
   // Construct the form data to be sent
-  // TODO: There is an error in this block of code preventing the audio being added
   const formData = new FormData();
   if (props.audioBlobURL !== null) {
     console.log(props.audioBlobURL);
     const audioBlob = await fetch(props.audioBlobURL).then((r) => r.blob());
-    // const audioFile = new File([audioBlob], 'voice.wav', { type: 'audio/wav' });
     formData.append('audio', audioBlob);
-    // fetch(props.audioBlobURL).then((response) => {
-    //   const blob = response.blob();
-    //   blob.then((blob) => {
-    //     formData.append('test', 'This is a test');
-    //     formData.append('audio', blob);
-    //   });
-    // });
   } else {
     console.log('Error: blob url not present');
   }
-  formData.append('test', 'This is a test');
+
   console.log(formData);
   // Send the form data
   axios
@@ -50,42 +42,6 @@ async function uploadAudio(props: AudioBlobProps) {
     .finally(function () {
       console.log('Cleaning up!');
     });
-}
-
-const AudioRecorder: React.FC = () => {
-  const {
-    startRecording,
-    stopRecording,
-    mediaBlobUrl,
-    status,
-    error,
-    isAudioMuted
-  } = useReactMediaRecorder({ audio: true });
-  console.log(isAudioMuted);
-  const audioBlobProps: AudioBlobProps = { audioBlobURL: mediaBlobUrl };
-  return (
-    <div>
-      <p>Status: {status}</p>
-      {error && <p>Error: {error}</p>}
-      <br />
-      <button onClick={startRecording}>Start Recording</button>
-      <br />
-      <button onClick={stopRecording}>Stop Recording</button>
-      <br />
-      {mediaBlobUrl && <audio src={mediaBlobUrl} controls />}
-      <br />
-      <button onClick={() => uploadAudio(audioBlobProps)}> Upload </button>
-    </div>
-  );
-};
-
-export function AudioTest() {
-  return (
-    <div>
-      <h1>Audio Recorder</h1>
-      <AudioRecorder />
-    </div>
-  );
 }
 
 export function AudioRecordButton({
