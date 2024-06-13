bebcaa83b7d7a3059976753dbc4aed84f3997d91
Luke Moran <lm1222@ic.ac.uk>
2024-06-13 23:40:03 +0100

    Fix invite bug by updating dependency array for subscribe

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { ThemeSubheading } from './theme/ThemeSubheading';
 import {
   updateEventResponse,
@@ -19,6 +19,7 @@ import toast, { Toaster } from 'react-hot-toast';
 import ConfettiExplosion from 'react-confetti-explosion';
 import { useLocation } from 'react-router-dom';
 import './Home.css';
+import { Unsubscribe } from 'firebase/firestore';
 
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 // const displayOldEvents = false;
@@ -242,7 +243,6 @@ export default function Home() {
   const [users, setUsers] = useState<PersonMap>({});
   const [currentUser, setCurrentUser] = useState<UID>(getCurrentUser());
   const [displayOldEvents, setDisplayOldEvents] = useState<boolean>(true);
-
   const location = useLocation();
   const [confetti, setConfetti] = useState(
     location.state ? location.state.confetti : false
@@ -257,16 +257,10 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    subscribeToEvents(getCurrentUser(), setEvents);
-  }, []);
+  useEffect(() => subscribeToEvents(currentUser, setEvents), [currentUser]);
 
   useEffect(() => {
     localStorage.setItem('user', currentUser);
-  }, [currentUser]);
-
-  useEffect(() => {
-    fetchEvents(currentUser).then(setEvents);
   }, [currentUser]);
 
   useEffect(() => {
