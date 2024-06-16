ac6b06658df9c8bb4dab957a050431356d69bd79
TomSmail <thomas.c.smail@gmail.com>
2024-06-16 18:18:21 +0100

    feat: add backend functionality to allow conversation

--- backend/app.py
+++ backend/app.py
@@ -8,40 +8,19 @@ client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 SLIM_TRANSCRIPTION = True
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="../frontend/build", index_file=True)
 
 @app.route('/api/post_audio', methods=['POST'])
 def audio_transcribe():
-    print(request.files)
-    if 'audio' not in request.files:
-        return jsonify({'error': 'No audio file attached in the request'}), 400
+    # Extract transcription
+    transcription = extract_transcript(request)
 
-    audio = request.files['audio']
-    if audio.filename == '':
-        return jsonify({'error': 'Audio filename is blank'}), 400
-
+    # Error if subject not present
     if 'subject' not in request.form:
         return jsonify({'error': 'No extraction type queried'}), 400
-
     subject = request.form['subject']
 
-    # Create a temporary file
-    temp = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
-    temp.write(audio.read())
-    temp.flush()  # Ensure all data is written to the file
-
-    # Get the temporary file path
-    temp_file_path = temp.name
-
-    print(temp_file_path)
-    
-    audio_file= open(temp_file_path, "rb")
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1", 
-        file=audio_file
-    )
-    result = transcription.text
     # Reduce the size of the transcription
     if SLIM_TRANSCRIPTION:
         transcription = client.chat.completions.create(
@@ -57,6 +36,74 @@ def audio_transcribe():
     print(result)
     return result
 
+@app.route('/api/post_conversation', methods=['POST'])
+def conversation_transcribe():
+    # Extract transcription
+    transcription = extract_transcript(request)
+
+    prompt = f"""
+        Extract the concise activity, people, time and place 
+        from the piece of text and return it as json with the following entries:
+            {{
+                "activity": the activity,
+                "people": list of people [],
+                "time": the time in the form hour/day,
+                "place": the place
+            }}
+        If it is unclear what to write fill in the gaps with 'NA'.
+        Do this on this text: {transcription}
+        
+        For example, given the piece of text:
+          I want to go to pub with bertha and gertrude tomorrow at 1
+        The result would be:
+            {{
+                "activity": "go to pub"
+                "people": ["Bertha", "Gertrude"]
+                "time": 1/Tomorrow,
+                "place": "pub"
+            }}
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "user",
+             "content": prompt}
+        ]
+    )
+    result = response.choices[0].message.content
+    return result
+
+
+
+# Extracts transcript from a request containing audio
+def extract_transcript(request):
+    print(request.files)
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No audio file attached in the request'}), 400
+
+    audio = request.files['audio']
+    if audio.filename == '':
+        return jsonify({'error': 'Audio filename is blank'}), 400
+
+    # Create a temporary file
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
+    temp.write(audio.read())
+    temp.flush()  # Ensure all data is written to the file
+
+    # Get the temporary file path
+    temp_file_path = temp.name
+
+    print(temp_file_path)
+
+    audio_file = open(temp_file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+
+    return transcription
 
 if __name__ == '__main__':
     app.run()
