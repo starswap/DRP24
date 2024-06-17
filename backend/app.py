3a77e562611c1b5be4f2880fd5a94c475d170815
TomSmail <thomas.c.smail@gmail.com>
2024-06-17 21:33:00 +0100

    feat: add full functionality to add events using only conversation

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import os
 
 from flask import Flask, jsonify, request
--- frontend/src/SpeechAccessibility/ConvsersationBot.tsx
+++ frontend/src/SpeechAccessibility/ConvsersationBot.tsx
@@ -2,12 +2,18 @@ import { useState, useEffect, useRef } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder-2';
 import axios, { Axios, AxiosResponse } from 'axios';
 import { ThemeButton } from '../theme/ThemeButton';
-import { CalendarEvent } from '../types/CalendarEvent';
+import { CalendarEvent, EventResponse } from '../types/CalendarEvent';
 import { MultiPageFormStateProps } from '../MultiPageForm/MultiPageForm';
-import { createEvent, createEventMeta } from '../util/data';
+import {
+  createEvent,
+  createEventMeta,
+  fetchUsers,
+  getCurrentUser
+} from '../util/data';
 import { Reschedule } from '../Home';
 import { useNavigate } from 'react-router-dom';
 import { EMPTY_EVENT } from './CreateConversationScreen';
+import { PersonMap } from '../types/Person';
 const AUDIO_START_TEXT = 'Press to speak answer';
 
 // If developing locally use set LOCAL env var
@@ -54,6 +60,8 @@ export function ConversationRecordButton({
   const navigate = useNavigate();
   const startRef = useRef(Date.now());
   const [recorded, setRecorded] = useState(false);
+  const [people, setPeople] = useState<PersonMap>({});
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const confirm = (currentEvent: CalendarEvent) => {
     const time_taken = Date.now() - startRef.current;
     console.log(`Time taken: ${time_taken}`);
@@ -69,6 +77,7 @@ export function ConversationRecordButton({
     navigate('/', { state: { confetti: true } });
   };
 
+  // Use the react media recorder to record audio
   const { startRecording, stopRecording } = useReactMediaRecorder({
     audio: true,
     onStop: (blobUrl) => {
@@ -76,26 +85,91 @@ export function ConversationRecordButton({
     }
   });
 
+  // Update the state of the conversation based on the response from the server
   function updateConversationState(response: AxiosResponse) {
     let new_state = state;
     // Check for each field in the response and update the state accordingly
+    // If activity is not present, set it to 'Something'
     if ('activity' in response.data && response.data['activity'] !== 'NA') {
       new_state = { ...new_state, activity: response.data['activity'] };
+    } else {
+      console.error('Error: activity not present in response');
+      new_state = { ...new_state, activity: 'Something' };
     }
+    // If place is not present, set it to 'Somewhere'
     if ('place' in response.data && response.data['place'] !== 'NA') {
       new_state = { ...new_state, location: response.data['place'] };
+    } else {
+      console.error('Error: place not present in response');
+      new_state = { ...new_state, location: 'Somewhere' };
     }
-    // if ('people' in response.data) {
-    //   new_state = { ...new_state, participants: response.data['people'] };
-    // }
-    // if ('time' in response.data && response.data['time'] !== 'NA') {
-    //   new_state = { ...new_state, time: response.data['time'] };
-    // }
+    // If people are not present do nothing as already set defaults
+    if ('people' in response.data) {
+      // Cycle through all users in response and add them to the event
+      for (const personOneName of response.data['people']) {
+        for (const [id, personTwo] of Object.entries(people)) {
+          // Check if the person is in the list of people
+          if (
+            personTwo.name.firstname.toLowerCase() ===
+            personOneName.toLowerCase()
+          ) {
+            console.log(`Found: ${personTwo.name.firstname}`);
+            // We shouldn't need to edit so there should only be one version of each person
+            new_state = {
+              ...new_state,
+              participants: new_state.participants.concat([id]),
+              statuses: {
+                ...new_state.statuses,
+                [id]: { person: personTwo, response: EventResponse.UNKNOWN }
+              }
+            };
+          }
+        }
+      }
+    } else {
+      console.error('Error: people not present in response');
+    }
+    // If time is not present, set it to tomorrow
+    if ('time' in response.data && response.data['time'] !== 'NA') {
+      const time = new Date(response.data['time']);
+      new_state = { ...new_state, time: time };
+    } else {
+      console.error('Error: time not present in response');
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1); // set the date to tomorrow
+      new_state = { ...new_state, time: tomorrow };
+    }
     console.log('New state updated');
     console.log(new_state);
     updateState(new_state);
   }
+  // Add the current user to the event automatically and have them accept it
+  useEffect(() => {
+    const curr = getCurrentUser();
+    if (curr in people) {
+      // if all users fetched
+      updateState((oldState) => ({
+        ...oldState,
+        participants: oldState.participants.concat([curr]),
+        statuses: {
+          ...oldState.statuses,
+          [curr]: { person: people[curr], response: EventResponse.ACCEPTED }
+        }
+      }));
+    }
+    console.log('Current User added to event');
+    console.log(state);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [people]);
 
+  // Fetch users from the database
+  useEffect(() => {
+    fetchUsers().then(setPeople);
+    console.log('Fetched users');
+    console.log(people);
+  }, []);
+
+  // When the mediaBlobUrl is updated, upload the audio to the server
   useEffect(() => {
     if (mediaBlobUrl) {
       const upload = async () => {
@@ -115,6 +189,7 @@ export function ConversationRecordButton({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mediaBlobUrl]);
 
+  // When the conversation is finished recording, confirm the conversation
   useEffect(() => {
     console.log(state);
     if (recorded) {
@@ -122,6 +197,7 @@ export function ConversationRecordButton({
     }
   }, [confirm, recorded, state]);
 
+  // When the button is clicked, start or stop recording
   const handleClick = () => {
     if (recording) {
       // Stop
